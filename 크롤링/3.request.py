3afde22959c69e04c45e0d2de610010c9e4ab497
hongdaniel <you@example.com>
2021-03-02 20:05:33 +0900

    크롤링 removed

--- 크롤링/3.request.py
+++
@@ -1,5 +0,0 @@
-import requests 
-
-res = requests.get("http://naver.com")
-res.raise_for_status()
-print("응답코드: ",res.status_code)
--- 크롤링/5.user_agent.py
+++
@@ -1,8 +0,0 @@
-import requests 
-
-
-url = "http://nadocoding.tistory.com"
-headers = {"user-agent : Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"}
-res = requests.get("http://nadocoding.tistory.com")
-res.raise_for_status()
-print("응답코드: ",res.status_code)
