0a88ada35cf19e3668246bc8fcce992d5d47d6b1
hongdaniel <you@example.com>
2021-03-03 15:50:30 +0900

    스킬트리

--- 프로그래머스/Level2/exp.py
+++ 프로그래머스/Level2/exp.py
@@ -1,1 +1,4 @@
-print('hello world')
+import string
+al = list(string.ascii_uppercase)
+
+print(al)
---
+++ 프로그래머스/Level2/스킬트리.py
@@ -0,0 +1,56 @@
+import string
+
+skill = "CBD"
+skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
+
+
+def solution(skill, skill_trees):
+    answer = 0
+    skill = list(skill)
+    skilldic = {}  # 스킬의 순서
+    al = list(string.ascii_uppercase)  # 대문자
+
+    for i in range(len(al)):  # 스킬에 인덱스 부여
+        if al[i] in skill:
+            skilldic[al[i]] = skill.index(al[i])+1
+        else:
+            skilldic[al[i]] = 0
+
+    for n in range(len(skill_trees)):
+        flag = True
+        maxn = 0
+
+        for i in range(len(skill_trees[n])):
+            if(maxn < skilldic[skill_trees[n][i]]):
+                if(skilldic[skill_trees[n][i]] == maxn+1):
+                    maxn = skilldic[skill_trees[n][i]]
+                else:
+                    flag = False
+
+        if(flag):
+            answer += 1
+
+    return answer
+
+
+def solution2(skill, skill_trees):
+    answer = 0
+    con = []
+    for n in range(len(skill_trees)):
+        con = []
+        flag = True
+        for i in range(len(skill_trees[n])):
+            if skill_trees[n][i] in skill:
+                con.append(skill_trees[n][i])
+        print(con)
+        new = "".join(con)
+        for i in range(len(new)):
+            if(new[i] != skill[i]):
+                flag = False
+
+        if(flag):
+            answer += 1
+    print(answer)
+
+
+solution2(skill, skill_trees)
