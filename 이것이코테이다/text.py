adad9270a5a482a3e01a5d061ffabdbf3492dbb0
hongdaniel <you@example.com>
2021-03-23 22:23:32 +0900

    시각.py

---
+++ 이것이코테이다/text.py
@@ -0,0 +1,4 @@
+sec = 34
+
+if '3' in str(sec):
+    print("str(sec))")
---
+++ 이것이코테이다/구현/시각.py
@@ -0,0 +1,12 @@
+hour = int(input())
+
+answer = 0
+count = 0
+for i in range(hour+1):
+    for m in range(60):
+        for sec in range(60):
+            if '3' in str(i)+str(m)+str(sec):
+                answer += 1
+
+
+print(answer)
