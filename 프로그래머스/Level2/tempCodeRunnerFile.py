6f6d8c89014e5cbfe134eff4f652d079d67d0734
hongdaniel <jungd1223@naver.com>
2021-04-08 21:07:09 +0900

    문자열 압축

--- 프로그래머스/Level2/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    # for row1 in range(len(arr1_row)):
-    #     for col1 in range(len(arr1_col)):
-    #         for row2 in range(len(arr2_row)):
-    #             for col2 in range(len(arr2_col)):
-    #                 answer[row1][col2] = arr1[row1][col1]*arr2[col2][row2]
