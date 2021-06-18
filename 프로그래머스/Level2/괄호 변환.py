4aef0920f2646eb9ceca0e9ad3045c629d64fffb
hongdaniel <jungd1223@naver.com>
2021-06-18 16:08:34 +0900

    재귀

--- 프로그래머스/Level2/괄호 변환.py
+++ 프로그래머스/Level2/괄호 변환.py
@@ -1,50 +1,63 @@
-p = "(()())()"
+p = "()))((()"
 
 
-def isright(s):
-    stack = []
-    for l in s:
-        if len(stack) == 0 and l == ')':
-            return False
+def correct(p):  # 올바른 문자열 확인 ok
+    tmp = 0
+    for l in p:
         if l == '(':
-            stack.append(l)
-        if stack[-1] == '(' and l == ')':
-            stack.pop()
-    if len(stack) == 0:
+            tmp += 1
+        else:
+            tmp -= 1
+        if tmp < 0:
+            return False
+    if tmp == 0:
         return True
     else:
         return False
 
 
-def solution(p):
-    answer = ''
-
-    while(1):
-        if len(p) == 0:  # 빈 문자열일 때
-            print(p)
-            return p
-        if len(p) == len(answer):
-            print(answer)
-            return answer
-
-        count = 0
-        u, v = ('', '')
-        for i in range(len(p)):  # u,v를 설정
-            if p[i] == '(':
-                count += 1
-            elif p[i] == ')':
-                count -= 1
-            if count == 0:
-                u = p[:i+1]
-                v = p[i+1:]
-                break
-        print(f"u {u}")
-        print(f"v {v}")
-
-        if isright(u):  # u가 올바를 때
-            p = v
-            continue
+def divide(p):  # u,v 나누는 과정 ok
+    if len(p) == 0:
+        return ''
+    dec = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            dec += 1
         else:
+            dec -= 1
+
+        if dec == 0:
+            u = p[:i+1]
+            v = p[i+1:]
+            break
+    return u, v
+
+
+def reverse(u):
+    u = list(u[1:-1])
+    for i in range(len(u)):
+        if u[i] == '(':
+            u[i] = ')'
+        else:
+            u[i] = '('
+    u = "".join(u)
+    return u
+
+
+def recursive(s):
+    if s == '':  # 1단계
+        return ''
+    u, v = divide(s)  # 2단계
+    # print(f"u: {u} v: {v}")
+    if correct(u):  # u가 올바른 문자열일 때
+        return u+recursive(v)
+
+    else:  # u가 아닐때
+        return '(' + recursive(v) + ')' + reverse(u)
+
+
+def solution(p):
+    return p if correct(p) else recursive(p)
 
 
 solution(p)
