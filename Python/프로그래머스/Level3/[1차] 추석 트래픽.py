1993f180dc2c614e58e48676c0773ce1334a2776
hongdaniel <jungd1223@naver.com>
2022-01-30 01:51:21 +0900

    구현

---
+++ Python/프로그래머스/Level3/[1차] 추석 트래픽.py
@@ -0,0 +1,13 @@
+lines = [
+    "2016-09-15 01:00:04.002 2.0s",
+    "2016-09-15 01:00:07.000 2s"
+]
+
+
+def solution(lines):
+    answer = 0
+    print(lines)
+    return answer
+
+
+solution(lines)
